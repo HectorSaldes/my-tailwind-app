099e429b1c20087cd1ed525f48ebe2d486f800e6
HectorSaldes <20193tn070@utez.edu.mx>
2022-02-05 10:32:18 -0600

    Ejemplo tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-9xl m-5">
+      <span className="text-transparent bg-clip-text bg-gradient-to-r  from-gray-800 to-blue-400 font-bold">
+        ¡Hemos logrado implementar TailwindCSS!
+      </span>
+      <span>👩🏻‍💻 🧑🏻‍💻</span>
     </div>
-  );
+  )
 }
-
-export default App;
